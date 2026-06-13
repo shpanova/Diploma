3f63d7487c4c7a9e7451bc92998b2a9fc2b353dd
Alexandra Panova <panova.alexandra04@yandex.ru>
2026-06-14 00:21:33 +0300

    test6

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
   output: {
   filename: '[name].[contenthash].js',
   path: path.resolve(__dirname, 'docs'),
-  publicPath: '/Diploma/',   // явный корень — на случай, если 'auto' не сработал
+  publicPath: '/',   // явный корень — на случай, если 'auto' не сработал
   },
   module: {
     rules: [
